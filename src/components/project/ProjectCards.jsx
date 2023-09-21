5aadacc49d0c1bc400d80a8f5c066110a1fd0b48
Adonias Fernandes Falcão <adoniasfalcao036@gmail.com>
2023-09-21 08:06:54 -0300

    Cards

---
+++ src/components/project/ProjectCards.jsx
@@ -0,0 +1,11 @@
+import styles from "./ProjectCards.module.css"
+import { BsPencil,BsFillTrashFill } from 'react-icons/bs'
+
+
+function ProjectCards ({ id,name,budget,category,handleRemove }) {
+    return (
+        <p>{name}</p>
+    )
+}
+
+export default ProjectCards
