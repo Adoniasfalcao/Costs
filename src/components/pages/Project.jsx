958ab0d33b3ac60077dec82d53fa95aec40766d9
Adonias Fernandes Falcão <adoniasfalcao036@gmail.com>
2023-09-22 14:05:17 -0300

    EditProjects

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,17 +1,38 @@
 import styles from './Pages style/Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState,useEffect } from 'react'
+import ProjectEdit from '../project/ProjectEdit'
 
 function Project () {
 
     const { id } = useParams()
     const [project,setProject] = useState([])
 
+    function editPost (project) {
+
+    }
+
+    useEffect( () => {
+
+        fetch(`http://localhost:5000/project/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+        }
+        )
+        .then( resp => resp.json() )
+        .then( data => setProject(data) )
+        .catch( err => console.log(err) )
+
+    },[])
 
 
     return(
-        <div>
-            {project.name}
+        <div className={styles.edit_project_container}>
+            <h1>Editar projeto</h1>
+            <ProjectEdit btnText={"Editar projeto"} handleSubmit={editPost}/>
+
         </div>
     )
 
---
+++ src/components/project/ProjectEdit.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+import styles from './ProjectEdit.module.css';
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+
+function ProjectForm({ btnText, handleSubmit, projectData }) {
+  const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+        console.log(data)
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const submit = (event) => {
+    event.preventDefault();
+    handleSubmit(project);
+  };
+
+  // Função para formatar o valor como moeda (BRL)
+  const formatCurrency = (value) => {
+    // Remova todos os caracteres não numéricos
+    const numericValue = value.replace(/\D/g, '');
+    const newNumericValue = numericValue.replace(/^(\S)(\S)(\S)(\S)(\S)(\S)/g, '$$1.$2$3$4,$5$6')
+    
+    // Verifique se o valor não está vazio
+    if (numericValue === '') {
+      return '';
+    }
+  
+    // Converta para um número2dsad
+    const floatValue = newNumericValue;
+    return floatValue
+    
+    // Formate apenas se for um número válido
+   
+  };
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const formattedValue = formatCurrency(newValue);
+    setProject({ ...project, project_budget: formattedValue });
+  }
+
+  function handleCategory(event) {
+    setProject({
+      ...project,
+      category: {
+        id: event.target.value,
+        name: event.target.options[event.target.selectedIndex].text,
+      },
+    });
+  }
+
+  return (
+    <form className={styles.form} onSubmit={submit}>
+      <Input
+        name="project_name"
+        type="text"
+        placeholder="Edite o nome do projeto"
+        text="Nome do projeto"
+        handleOnChange={(event) =>
+          setProject({ ...project, project_name: event.target.value })
+        }
+        value={project.project_name}
+      />
+      <Input
+        name="project_budget"
+        type="text"
+        placeholder="Insira novo orçamento"
+        text="Orçamento"
+        handleOnChange={handleChange}
+        value={project.project_budget}
+      
+      
+      />
+      <Select
+        text="Selecione a nova categoria"
+        name="select_category"
+        options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ''}
+      />
+      <SubmitButton text={btnText} />
+    </form>
+  );
+}
+
+export default ProjectForm;
