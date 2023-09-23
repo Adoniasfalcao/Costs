2db2f3d0574d1777d4396855c0fccfe474023fa7
Adonias Falcão <adoniasfalcao036@gmail.com>
2023-09-23 17:56:21 -0300

    Teste CSS

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -6,9 +6,11 @@ function Company () {
         <div className={styles.company_container}>
             <h1>Empresa</h1>
             <img src={logo} alt='Costs'/>
-            <p>Somos uma empresa de gerenciamento de projetos altamente especializada e que almeja atingir <span>sucesso</span> no nosso setor.</p>
-            <p>Iniciamos nossa jornada no ano de 2023 com o objetivo de facilitar a criação e o planejamento de projetos dos nossos clientes, pensando trouxemos uma <span>solução inovadora 
-                </span> e de <span>simples</span> utilização para garantir maior <span>agilidade</span> e <span>eficiência</span> na gerência de projetos</p>
+            <p>Somos uma empresa de gerenciamento de projetos altamente especializada e que almeja atingir <span> sucesso </span> no nosso setor.</p>
+            <p>
+                Iniciamos nossa jornada no ano de 2023 com o objetivo de facilitar a criação e o planejamento de projetos dos nossos clientes,pensando nisso trouxemos uma <span>solução inovadora</span> e de <span>simples</span> utilização para garantir maior <span>agilidade</span> e <span>eficiência</span> na gerência de projetos
+            </p>
+
         </div>
     )
 }
