f4dead6c7a91b0e9876c45d382c12867a194ebe9
Adonias Fernandes Falcão <adoniasfalcao036@gmail.com>
2023-09-28 08:12:52 -0300

    Validação dos serviços

--- src/components/services/ServiceForm.jsx
+++ src/components/services/ServiceForm.jsx
@@ -6,14 +6,24 @@ import styles from '../project/ProjectForm.module.css'
 
 function ServiceForm ({ handleSubmit, btnText, projectData }) {
 
-    function submit () {
+    const [service, setService] = useState({})
 
-    }
+
+    //Enviar dados
+    function submit (e) { 
+        e.preventDefault();
+        projectData.services.push(service)
+        handleSubmit(projectData)   
+    };
 
 
     function handleChange (e) {
 
+        if (e.target.value != '') {
+            setService( {...service, [e.target.name]: e.target.value})
+        }
     }
+
 
     return(
         <form onSubmit={submit} className={styles.form}>
